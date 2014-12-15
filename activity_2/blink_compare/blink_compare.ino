10266b834247a56f3c3dc0fae03473ff2b086de4
1sc0bot <x1sc0@acrobotic.com>
2014-12-14 16:41:22 -0800

    updating code comments

--- activity_2/blink_compare/blink_compare.ino
+++ activity_2/blink_compare/blink_compare.ino
@@ -1,9 +1,25 @@
-/*
-  Blink
- */
+/*-----------------------------------------------------------------------
+  Acrobotic - 01/12/2014
+  Author: x1sc0
+  Platforms: Arduino Uno R3
+  File: blink_compare.ino
+  ------------------------------------------------------------------------
+  Description: 
+  We compare a fully on LED against one that is blinking very rapidly 
+  (imperceptible to our eyes at a first glance)
+  ------------------------------------------------------------------------
+  Please consider buying products from Acrobotic to help fund future
+  Open-Source projects like this! We’ll always put our best effort in every
+  project, and release all our design files and code for you to use. 
+  http://acrobotic.com/
+  ------------------------------------------------------------------------
+  License:
+  Beerware License; if you find the code useful, and we happen to cross 
+  paths, you're encouraged to buy us a beer. The code is distributed hoping
+  that you in fact find it useful, but  without warranty of any kind.
+------------------------------------------------------------------------*/
  
-// We compare a fully on LED against one that is blinking very rapidly 
-// (imperceptible to our eyes at a first glance)
+// Initialize 2 variables for the pins we'll be using later
 int led_on = 13;
 int led_flicker = 12;
 
@@ -12,7 +28,7 @@ void setup() {
   // initialize the digital pin as an output.
   pinMode(led_on, OUTPUT);
   pinMode(led_flicker, OUTPUT);       
-  digitalWrite(led_on, HIGH);   // turn the LED on (HIGH is the voltage level)  
+  digitalWrite(led_on, HIGH);   // turn the LED on
 }
 
 // the loop routine runs over and over again forever:
--- activity_2/heartbeat/heartbeat.ino
+++ activity_2/heartbeat/heartbeat.ino
@@ -1,29 +1,59 @@
+/*-----------------------------------------------------------------------
+  Acrobotic - 01/12/2014
+  Author: x1sc0
+  Platforms: Arduino Uno R3
+  File: heartbeat.ino
+  ------------------------------------------------------------------------
+  Description: 
+  We compare a fully on LED against one that is blinking very rapidly 
+  (imperceptible to our eyes at a first glance)
+  ------------------------------------------------------------------------
+  Please consider buying products from Acrobotic to help fund future
+  Open-Source projects like this! We’ll always put our best effort in every
+  project, and release all our design files and code for you to use. 
+  http://acrobotic.com/
+  ------------------------------------------------------------------------
+  License:
+  Beerware License; if you find the code useful, and we happen to cross 
+  paths, you're encouraged to buy us a beer. The code is distributed hoping
+  that you in fact find it useful, but  without warranty of any kind.
+------------------------------------------------------------------------*/
+
+// Initialize variable with the pin number which we'll be using. Ensure
+// that it's a PWM pin.
 int led_pin = 11;
 
-int value = 0,
-    value_max = 128,
-    number_of_steps = 255,
-    step_delay_ms = 5,
-    cycle_delay_ms = 1000;
+// Initialize a few variables that we'll be using in our loop function
+// for controlling LED brightness
+int value = 0, // current brigthness
+    value_max = 128,  // maximum brightness (brightness doesn't change 
+                      // much above this value)
+    number_of_steps = 256, // steps taken between fully OFF and fully ON
+    step_delay_ms = 5, // wait 5ms at each intensity value
+    cycle_delay_ms = 1000; // stay off for 1000ms between cycles
 
 void setup()
 {
   pinMode(led_pin, OUTPUT);
 }
 
+// Most variables can be initialized outside the setup and loop functions
+// though above the point where they're first used
 int step_size = max( (int)value_max/number_of_steps, 1 );
 
 void loop()
 {
-  analogWrite(led_pin, value);  
-  delay(step_delay_ms);  
-  value+=step_size;
+  analogWrite(led_pin, value); //set the current LED brightness  
+  delay(step_delay_ms); //hold the value for a short time
+  value+=step_size; //increase the current value for the next iteration
 
-  if( (value >= value_max)||(value<0) )
-    step_size *= -1;
-  if(value < 0)
+  // if we've reached the maximum or minimum value, change direction of
+  // change in brightness (i.e., increasing -> decreasing or viceversa)
+  if( (value>value_max)||(value<0) )
   {
-    value = 0;
-    delay(cycle_delay_ms);
+    step_size *= -1;
+    value+=step_size; // ensure that value stays within bounds
+    if(value == 0)
+      delay(cycle_delay_ms); // hold 1 second off starting each cycle
   }
 }
