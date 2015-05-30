8f6cb2ea6cce97954596c20d32240233a839c8f1
1sc0bot <x1sc0@acrobotic.com>
2015-05-30 16:41:57 -0700

    updating

--- activity_02/blink_external/blink_external.ino
+++ activity_02/blink_external/blink_external.ino
@@ -1,8 +1,27 @@
-/* 
-Blink Turns on an LED on for one second, 
-then off for one second, repeatedly. This 
-example code is in the public domain. 
-*/
+/*-----------------------------------------------------------------------
+  Acrobotic - 01/12/2014
+  Author: x1sc0
+  Platforms: Arduino Uno R3
+  File: blink_external.ino
+  ------------------------------------------------------------------------
+  Description: 
+  This program is identical to the built-in Blink example. The idea is
+  for those using an Arduino Uno board to connect an external LED using 
+  the accompanying wiring diagram as a guide.
+  
+  We'll observe that when using pin 13, both the built-in LED, as well as
+  the externally connected one will blink simultaneously!
+  ------------------------------------------------------------------------
+  Please consider buying products from Acrobotic to help fund future
+  Open-Source projects like this! We’ll always put our best effort in every
+  project, and release all our design files and code for you to use. 
+  http://acrobotic.com/
+  ------------------------------------------------------------------------
+  License:
+  Beerware License; if you find the code useful, and we happen to cross 
+  paths, you're encouraged to buy us a beer. The code is distributed hoping
+  that you in fact find it useful, but  without warranty of any kind.
+------------------------------------------------------------------------*/
 // Using an external LED, we'll notice that both the internal 
 // and external LEDs blink at the same rate!
 int led = 13; 
